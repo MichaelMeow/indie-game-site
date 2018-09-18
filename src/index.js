66fe9c7a8cb65fe313a21598eacd16d7c7667cb9
Joe Friesen and Michael Harmon <joe@>
2018-09-18 13:40:38 -0700

    Initialize index.html with basic CSS grid structure.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@
 import './sass/styles.scss';
 
 console.log("hi.");
+
+// Creating a countdown timerbackground. This, uh, it doesn't really work yet.
+
+var releaseDate = new Date("Sept 21, 2018 00:00:00").getTime();
+
+var time = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = releaseDate - now;
+  var days = Math.floor(distance / (1000*60*60*24));
+  var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+  var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+  var seconds = Math.floor((distance & (1000* 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(time);
+    document.getElementById("timer").innerHTML = "Spooky Scary Forest Is Out NOW!";
+  }
+}, 1000);
